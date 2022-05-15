c76809d5149aee87605b8afb6e12006092cb7563
MaiteCabrera <maitecabrera11@gmail.com>
2022-05-15 17:20:23 -0300

    Hasta aca todo bien

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'proyecto-angular1';
 }
+/*
+Un controlador o Controller: 
+Es dónde se encuentra la lógica, este archivo incluye una clase 
+y esa es la que contiene las propiedades que se van a usar en la vista de HTML. 
+Los métodos que serán las acciones que se ejecutarán en la vista. 
+Se incluye una metadata, es que definida con un decorador, que identifica 
+a Angular como un componente. 
+*/
